97ab0043856bca538f1706453833a7ea881714b0
Sarah E <seatherly.prsvr@gmail.com>
2025-06-17 11:49:31 -0500

    feat: add docker-compose.yml and update README.md

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpackDevMiddleware: (config: any) => {
+    config.watchOptions = {
+      poll: 1000,            // force polling
+      aggregateTimeout: 300, // reduce delay
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
